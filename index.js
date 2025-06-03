5671eb31844f39d8acdd8aa27c8e3cf5a615f37a
Joseph-Hansel <abogejosephhansel@gmail.com>
2025-06-03 16:01:39 +0300

    Finish Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = "USER 1"
+let welcomeMessage = "Welcome to Flatbook, USER 1!"
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, USER 1!"
+let shortGreeting = "Welcome, U!"
